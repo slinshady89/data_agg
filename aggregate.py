bf77e9a8eb650881b0570ec3d54c7687325d332d
Nils Hamacher <hamacher.nils@stud.tu-darmstadt.de>
2019-08-05 15:07:08 +0200

    working on data agg -> data preprocessing again

--- aggregate.py
+++ aggregate.py
@@ -17,12 +17,6 @@ def create_working_dir(args):
     return path
 
 
-def iou_for_semantic_class(target_color_channel, inferenced_color_channel):
-    intersection = np.logical_and(target_color_channel, inferenced_color_channel)
-    union = np.logical_or(target_color_channel, inferenced_color_channel)
-    return np.sum(intersection) / np.sum(union)
-
-
 def main(args):
 
     # image_name = os.path.join("%06d" % 1)  # 000001
@@ -35,62 +29,38 @@ def main(args):
     gt_labels = {}
     gt_labels['images'] = []
 
-    # for seq in seq_list:
-    #     gt_base_path = args.base_dir + seq + '/' + args.gt_labels
-    #     for gt_label in os.listdir(gt_base_path):
-    #         gt_labels['images'].append({'name': gt_label, 'sequence': seq})
-    #
-    # print(seq_list)
-    # print('Length of ground truth labeled images %d\n\n' % len(gt_labels['images']))
-    #
-    # shuffle(gt_labels['images'])
-    #
-    # with open(args.base_dir + 'train_list.json', "w") as f:
-    #     json.dump(gt_labels, f)
-    #
-    # with open(args.base_dir + 'train_list.json', 'r') as file:
-    #     file_list = json.load(file)
-    #
-    # for counter, item in enumerate(file_list['images']):
-    #     if counter > 10:
-    #         break
-    #     print(item['name'])
-    #     print(item['sequence'])
-    #
-    # batch1 = file_list['images'][:10]
-    # print(batch1)
-    #
-    # for name in batch1:
-    #     print(name['name'])
+    for seq in seq_list:
+        gt_base_path = args.base_dir + seq + '/' + args.gt_labels
+        for gt_label in os.listdir(gt_base_path):
+            gt_labels['images'].append({'name': gt_label, 'sequence': seq})
 
-    # # use only the first 10% of the all images to train the net
-    #train_list = gt_labels['images'][:int(len(gt_labels['images'])*0.0003)]
+    print(seq_list)
+    print('Length of ground truth labeled images %d\n\n' % len(gt_labels['images']))
 
-    # print(file_list["images"])
+    shuffle(gt_labels['images'])
 
+    with open(args.base_dir + 'train_list.json', "w") as f:
+        json.dump(gt_labels, f)
 
-    gt_labels = os.listdir(args.base_dir + '08/' + args.gt_labels)
-    inf_labels = os.listdir(args.base_dir + '08/20190730_170755/')
+    with open(args.base_dir + 'train_list.json', 'r') as file:
+        file_list = json.load(file)
 
-    r = g = b = 0.0
+    for counter, item in enumerate(file_list['images']):
+        if counter > 10:
+            break
+        print(item['name'])
+        print(item['sequence'])
 
-    for name in gt_labels:
-        gt_img = cv2.imread(args.base_dir + '08/' + args.gt_labels + name)
-        h, w, _ = gt_img.shape
-        cropped_img = gt_img[(h - 256) :h, ((w - 1024) // 2) : (w - (w - 1024) // 2)]
-        gt_img = cv2.resize(cropped_img, (1024, 256))
-        inf_img = cv2.imread(args.base_dir + '08/20190730_170755/' + name)
-        r += iou_for_semantic_class(gt_img[:, :, 0], inf_img[:, :, 0])
-        g += iou_for_semantic_class(gt_img[:, :, 1], inf_img[:, :, 1])
-        b += iou_for_semantic_class(gt_img[:, :, 2], inf_img[:, :, 2])
-        print('\r\033[1A\033[0K %d of %d' % (int(name.replace('.png', '')), len(gt_labels)))
+    batch1 = file_list['images'][:10]
+    print(batch1)
 
-    r /= len(gt_labels)
-    g /= len(gt_labels)
-    b /= len(gt_labels)
+    for name in batch1:
+        print(name['name'])
 
-    print(r, g, b)
+    # use only the first 10% of the all images to train the net
+    train_list = gt_labels['images'][:int(len(gt_labels['images'])*0.0003)]
 
+    print(file_list["images"])
 
 
 
