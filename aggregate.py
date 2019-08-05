2ef63c6fa7fe5c6e3d911b5dfa9eaf5d16de28bb
Nils Hamacher <hamacher.nils@stud.tu-darmstadt.de>
2019-08-05 18:26:10 +0200

    added something?

--- aggregate.py
+++ aggregate.py
@@ -6,12 +6,16 @@ from random import shuffle
 import json
 
 
-def create_working_dir(args):
-    path = args.base_dir + args.sequence + args.net_date
+def create_working_dir(args, folder_name):
+    path = args.base_dir + folder_name
     try:
         os.mkdir(path)
     except OSError as e:
-        print('OSError: %s', e)
+        if e.errno == 17:
+            print(path)
+            print('Directory exists already! Everything fine!')
+        else:
+            print('OSError: %s', e)
     else:
         print("Successfully created the directory %s " % path)
     return path
@@ -26,18 +30,23 @@ def main(args):
         if not i == 8:
             seq_list.append(os.path.join('%02d' % i))
 
+
+    images = 'images'
+    name = 'name'
+    sequence = 'sequence'
+    
     gt_labels = {}
-    gt_labels['images'] = []
+    gt_labels[images] = []
 
     for seq in seq_list:
         gt_base_path = args.base_dir + seq + '/' + args.gt_labels
         for gt_label in os.listdir(gt_base_path):
-            gt_labels['images'].append({'name': gt_label, 'sequence': seq})
+            gt_labels[images].append({name: gt_label, sequence: seq})
 
     print(seq_list)
-    print('Length of ground truth labeled images %d\n\n' % len(gt_labels['images']))
+    print('Length of ground truth labeled images %d\n\n' % len(gt_labels[images]))
 
-    shuffle(gt_labels['images'])
+    shuffle(gt_labels[images])
 
     with open(args.base_dir + 'train_list.json', "w") as f:
         json.dump(gt_labels, f)
@@ -45,26 +54,15 @@ def main(args):
     with open(args.base_dir + 'train_list.json', 'r') as file:
         file_list = json.load(file)
 
-    for counter, item in enumerate(file_list['images']):
-        if counter > 10:
-            break
-        print(item['name'])
-        print(item['sequence'])
+    test_path = create_working_dir(args, 'test/')
 
-    batch1 = file_list['images'][:10]
-    print(batch1)
-
-    for name in batch1:
-        print(name['name'])
-
-    # use only the first 10% of the all images to train the net
-    train_list = gt_labels['images'][:int(len(gt_labels['images'])*0.0003)]
-
-    print(file_list["images"])
-
-
-
-
+    for i, item in enumerate(gt_labels[images]):
+        # if i > 10:
+        #     break
+        img = cv2.imread(args.base_dir + item[sequence] + '/' + args.gt_labels + item[name])
+        status = False
+        while not status:
+            status = cv2.imwrite(test_path + item[sequence] + '_' + item[name], img)
 
 
 if __name__ == "__main__":
