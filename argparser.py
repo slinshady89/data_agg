b49878fd70aa68a1c5afb759f0d77119aba069bc
Nils Hamacher <hamacher.nils@stud.tu-darmstadt.de>
2019-08-22 16:26:53 +0200

    change path to Data

--- argparser.py
+++ argparser.py
@@ -6,21 +6,22 @@ def argparser():
     parser = argparse.ArgumentParser(description = 'Segmentation U-Net inference. '
                                                    'Predicting drivable paths in input images and highlight obstacles')
     parser.add_argument("--base_dir",
-                        default = '/media/localadmin/BigBerta/11Nils/kitti/dataset/sequences/',
+                        default = '/media/localadmin/BigBerta/11Nils/kitti/dataset/sequences/Data/',
                         help = "base dir where labels are found")
     parser.add_argument("--sequence",
                         default = '08/',
                         help = "Sequence in case of KITTI data")
     parser.add_argument("--images",
-                        default = '/image_2/',
+                        default = '/images/',
                         help = "loading dir of true image")
     parser.add_argument("--gt_labels",
-                        default = '/labels_gt/',
+                        default = '/labels/',
                         help = "loading dir of created labels by lidar and tf")
     parser.add_argument("--inf_labels",
                         # default = '20190729_164658/',
-                        default = '20190726_130705/',
-                        help = "loading dir of labely created by inference")
+                        # default = '20190726_130705/',
+                        default = 'inference/',
+                        help = "loading dir of labels created by inference")
 
     args = parser.parse_args()
 
