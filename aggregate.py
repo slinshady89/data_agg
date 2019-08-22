50fe9f4ac9e558e2947b73cac3bc837f76dce7e6
Nils Hamacher <hamacher.nils@stud.tu-darmstadt.de>
2019-08-22 16:27:51 +0200

    added first impressions of Aggregator list of dicts instead of dict of list of dicts

--- aggregate.py
+++ aggregate.py
@@ -5,6 +5,7 @@ import os
 from random import shuffle
 import json
 from operator import itemgetter
+import collections
 
 
 def create_working_dir(args, folder_name):
@@ -19,73 +20,74 @@ def create_working_dir(args, folder_name):
     return path
 
 
+class Aggregator(object):
+    def __init__(self):
+        self.dag_it_num = 0
+        self.base_dir = '/media/localadmin/BigBerta/11Nils/kitti/dataset/Data/'
+        self.img_dir = 'images/'
+        self.label_dir = 'labels/'
+        self.inf_dir = 'inference/'
+        self.dag_dir = 'dagger/'
+        self.img_name = 'name'
+        self.sequence = 'seq'
+        self.precision = 'prec'
+        self.recall = 'rec'
+        self.trained = 'trained'
+        self.pr = 'precision_times_recall'
+        self.dag_it = 'dagger_iteration'
+        self.img_list = []
+
+    def create_working_dir(self, folder_name):
+        path = args.base_dir + folder_name
+        try:
+            os.mkdir(path)
+        except OSError as e:
+            if e.errno != 17:
+                print('OSError: %s', e)
+        else:
+            print("Successfully created the directory %s " % path)
+        return path
+
+    def on_new_iter(self):
+        self.create_working_dir(os.path.join('%02d/' % self.dag_it_num))
+        self.create_working_dir(os.path.join('%02d/inf/' % self.dag_it_num))
+        label_path = self.base_dir + self.label_dir
+        # for label_name in os.listdir(label_path):
+        self.dag_it += 1
+
+
 def main(args):
     # image_name = os.path.join("%06d" % 1)  # 000001
 
-    seq_list = []
-    for i in range(0, 21):
-        if not i == 8:
-            seq_list.append(os.path.join('%02d' % i))
+    seq_list = [os.path.join('%02d' % i) for i in range(0, 22)]
 
     images = 'images'
     name = 'name'
     sequence = 'sequence'
-    iou_green = 'iou_green'
+    prec_green = 'prec_green'
+    rec_green = 'rec_green'
+    training = 'trained'
+    valid = 'valid'
+    pr = 'precision_times_recall'
+    dag_it = 'dagger_iteration'
 
-    gt_labels = dict()
-    gt_labels[images] = []
+    gt_labels = []
 
-    for seq in seq_list:
-        gt_base_path = args.base_dir + seq + args.gt_labels
-        for gt_label in os.listdir(gt_base_path):
-            gt_labels[images].append({name: gt_label, sequence: seq, iou_green: 0.00})
+    gt_base_path = args.base_dir + args.gt_labels
+    print(gt_base_path)
+    for gt_label in os.listdir(gt_base_path):
+        gt_labels.append({name: gt_label, prec_green: -1.0, rec_green: -1.0,
+                          training: False, valid: False, pr: 2.0, dag_it: -1})
 
+    test = gt_labels[0:10]
+
+    sorted_list = sorted(test, key = itemgetter(name), reverse = True)
+    print(sorted_list)
+
+    print('test')
     print(seq_list)
-    print('Length of ground truth labeled images %d\n\n' % len(gt_labels[images]))
+    print('Length of ground truth labeled images %d\n\n' % len(gt_labels))
 
-    shuffle(gt_labels[images])
-
-    test_list = dict()
-    test_list[images] = []
-
-    test_list[images] = gt_labels[images][0:20]
-
-    with open(args.base_dir + 'Data/test_list.json', "w") as f:
-        json.dump(test_list, f)
-
-    with open(args.base_dir + 'Data/test_list.json', "r") as f:
-        test_read = json.load(f)
-
-    shuffle(test_read[images])
-
-    for i, label in enumerate(test_read[images]):
-        if i >= 10:
-            break
-        label[iou_green] = 0.1
-
-    with open(args.base_dir + 'Data/test_read.json', "w") as f:
-        json.dump(test_read, f)
-
-    count = 0
-    for i in range(0, len(test_list[images]) - 1):
-        for j in range(0, len(test_read[images]) - 1):
-            # if test_read[images][j][iou_green] > 0:
-            if (test_list[images][i][name] == test_read[images][j][name]) and \
-                    (test_list[images][i][sequence] == test_read[images][j][sequence]):
-                # print('\nFound match at %d and %d:' % (i, j))
-                # print(test_list[images][i], test_read[images][j])
-                count += 1
-
-    print('\nFound %d matches\n' % count)
-
-    sorted_list = sorted(test_read[images], key = itemgetter(iou_green), reverse = True)
-
-    ix = np.random.choice(np.arange(len(sorted_list)), 8)
-    print(len(sorted_list))
-    print(np.arange(len(sorted_list)))
-
-    for i in range(0, len(sorted_list) - 1):
-        print(sorted_list[i])
 
 
 if __name__ == "__main__":
