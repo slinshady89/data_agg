a18b58c8bc002e830dc719b156f7d748012ff15f
Nils Hamacher <hamacher.nils@stud.tu-darmstadt.de>
2019-08-26 16:33:06 +0200

    train and validation lists are created correct now TODO: implement a test to check if no new images could be aggregated

--- aggregate.py
+++ aggregate.py
@@ -1,62 +1,60 @@
-from argparser import argparser
-import os
-from random import shuffle
 import json
 from operator import itemgetter
+from argparser import argparser
 from aggregator import Aggregator
-
-
-def create_working_dir(args, folder_name):
-    path = args.base_dir + folder_name
-    try:
-        os.mkdir(path)
-    except OSError as e:
-        if e.errno != 17:
-            print('OSError: %s', e)
-    else:
-        print("Successfully created the directory %s " % path)
-    return path
-
+from random import shuffle, randint
 
 
 def main(args):
-    seq_list = [os.path.join('%02d' % i) for i in range(0, 22)]
-
-    gt_labels = []
+    # seq_list = [os.path.join('%02d' % i) for i in range(0, 22)]
 
     aggregator = Aggregator()
-    print('Num of ground truth labeled images %d\n\n' % len(aggregator.img_list))
+    print('Num of ground truth labeled images %d\n\n' % len(aggregator.agg_list))
 
-    train_batch_len = int(len(aggregator.img_list) * 0.1 * 0.8)
-    val_batch_len = int(len(aggregator.img_list) * 0.1 * 0.2)
+    aggregator.sort_agg_list()
 
-    train_batch = aggregator.img_list[0:train_batch_len]
-    val_batch = aggregator.img_list[train_batch_len:train_batch_len + val_batch_len]
+    for i in range(0, 100):
+        print('\nDAgger Iteration: %d\n' % aggregator.dag_it_num)
+        aggregator.on_new_iter()
+        train, val = aggregator.get_training_data()
+        aggregator.save_list(train, 'train')
+        aggregator.save_list(val, 'val')
+        print('Length train set %d' % len(train))
+        print('Length validation set %d' % len(val))
+        if aggregator.images_evaluated == len(aggregator.agg_list):
+            print('Stopping DAgger because no new Data could be aggregated.\nCreate more!')
+            break
+        aggregator.aggregate()
 
-    print(len(train_batch))
-    print(len(val_batch))
-    print(len(aggregator.img_list[train_batch_len + val_batch_len:]))
+    test = aggregator.agg_list[0:100]
+    len_train_total = int(len(aggregator.agg_list[0:100]) * 0.1)
+    train_batch_len = int(len_train_total * 0.8)
+    val_batch_len = len_train_total - train_batch_len
 
-    len_all = len(train_batch) + len(val_batch) + len(aggregator.img_list[train_batch_len + val_batch_len:])
-    print('len all 3 together %d' % len_all)
+    train_batch = test[0:train_batch_len]
+    val_batch = test[train_batch_len:train_batch_len + val_batch_len]
 
-    test = aggregator.img_list[0:10]
+    # shuffle worst images after evaluation that belong to training data in the next iteration
+    # so they are added to training and validation with no order
 
-    for i in range(len(test)):
-        test[i][aggregator.k_dag_it] = i
+    for j in range(0, 9):
+        for i in range(len_train_total * j, len_train_total + len_train_total * j):
+            test[i][aggregator.k_dag_it] = j
+            test[i][aggregator.k_pr] = randint(0, 100) / 100
+            test[i][aggregator.k_quota_g] = randint(5, 12) / 100.0
+        aggregator.dag_it_num = j
 
     shuffle(test)
-    # for i in range(len(test)):
-    #     print(test[i])
 
-    test[3:10] = sorted(test[3:10], key = itemgetter(aggregator.k_dag_it), reverse = False)
+    test = aggregator.sort_agg_list(test)
     #
-    # print('\n')
     # for i in range(len(test)):
-    #     print(test[i])
+    #         print('%d | %s' % (i, test[i]))
 
-    sorted_list = sorted(test, key = itemgetter(aggregator.k_dag_it), reverse = True)
 
+    test[10:100] = sorted(test[10:100], key = itemgetter(aggregator.k_dag_it), reverse = False)
+
+    # aggregator.save_list(test)
 
 
 if __name__ == "__main__":
--- aggregator.py
+++ aggregator.py
@@ -1,7 +1,8 @@
-
+import json
 import os
-from random import shuffle
-
+from math import inf
+from random import shuffle, randint
+from operator import itemgetter
 
 
 class Aggregator(object):
@@ -16,17 +17,46 @@ class Aggregator(object):
         self.k_sequence = 'seq'
         self.k_precision = 'prec'
         self.k_recall = 'rec'
-        self.k_trained = 'trained'
+        self.k_quota_g = 'quota_green_gt'
+        # evaluation metric
         self.k_pr = 'precision_times_recall'
         self.k_dag_it = 'dagger_iteration'
-        self.k_training = 'training_data'
-        self.k_valid = 'validation_data'
-        self.img_list = []
-
+        # self.k_training = 'training_data'
+        # self.k_valid = 'validation_data'
+        self.train_perc = 0.8
+        self.val_perc = 0.2
+        self.train_batch_perc = 0.1  # 0.0005
+        self.agg_batch_perc = self.train_batch_perc / 2
+        self.agg_list_name = self.base_dir + self.dag_dir + 'agg_list.json'
+        self.agg_list = []
+        # add a dictionary entry for every label image in label_dir with initial values for evaluation metrics
+        # and key values if they belong to training or validation data of which DAgger iteration
+        # training and validation data accumulates older iterations
+        # images with green quota lower than 0.07 aren't further processed
         for gt_label in os.listdir(self.base_dir + self.label_dir):
-            self.img_list.append({self.k_img_name: gt_label, self.k_precision: -1.0, self.k_recall: -1.0,
-                                 self.k_training: False, self.k_valid: False, self.k_pr: 2.0, self.k_dag_it: -1})
-        shuffle(self.img_list)
+            self.agg_list.append({self.k_img_name: gt_label, self.k_precision: -1.0, self.k_recall: -1.0,
+                                  self.k_pr: 2.0, self.k_dag_it: 200, self.k_quota_g: randint(6, 12) / 100})
+
+        self.len_agg_list = len(self.agg_list)
+        shuffle(self.agg_list)
+        self.len_train_batch = int(self.len_agg_list * self.train_batch_perc * self.train_perc)
+        self.len_val_batch = int(self.len_agg_list * self.train_batch_perc * self.val_perc)
+        self.len_agg_train = self.len_train_batch // 2
+        self.len_agg_val = self.len_val_batch // 2
+        self.len_train_set = self.len_train_batch + self.len_val_batch
+        self.len_agg_set = self.len_agg_train + self.len_agg_val
+        self.num_imgs_to_train = self.len_train_set + self.len_agg_set * self.dag_it_num
+
+        with open(self.agg_list_name, "w") as f:
+            json.dump(self.agg_list, f)
+        f.close()
+        for i in range(self.len_train_set):
+            self.agg_list[i][self.k_dag_it] = 0
+
+        self.train_data_total = self.len_train_batch + self.len_val_batch
+        self.agg_data_total = self.len_agg_train + self.len_agg_val
+        self.on_new_iter()
+        self.images_evaluated = 0
 
     def create_working_dir(self, folder_name):
         path = self.base_dir + folder_name
@@ -35,13 +65,67 @@ class Aggregator(object):
         except OSError as e:
             if e.errno != 17:
                 print('OSError: %s', e)
-        else:
-            print("Successfully created the directory %s " % path)
         return path
 
     def on_new_iter(self):
-        self.create_working_dir(os.path.join('%02d/' % self.dag_it_num))
-        self.create_working_dir(os.path.join('%02d/inf/' % self.dag_it_num))
-        label_path = self.base_dir + self.label_dir
-        # for label_name in os.listdir(label_path):
-        self.k_dag_it += 1
+        self.create_working_dir(os.path.join(self.dag_dir))
+        self.create_working_dir(os.path.join(self.dag_dir + '%02d' % self.dag_it_num))
+        self.create_working_dir(os.path.join(self.dag_dir + '%02d/inf/' % self.dag_it_num))
+        self.agg_list_name = self.base_dir + self.dag_dir + '%02d/' % self.dag_it_num \
+                             + 'agg_list_%02d.json' % self.dag_it_num
+        self.num_imgs_to_train = self.len_train_batch + self.len_val_batch + \
+                                 (self.len_agg_train + self.len_agg_val) * self.dag_it_num
+
+    def aggregate(self):
+        self.dag_it_num += 1
+        pass
+
+    def sort_agg_list(self, list_to_sort = None):
+        if list_to_sort is None:
+            # self.agg_list = sorted(sorted(self.agg_list, key = itemgetter(self.k_dag_it), reverse = False),
+            #                        key = itemgetter(self.k_pr), reverse = False)
+            self.agg_list = sorted(self.agg_list, key = itemgetter(self.k_dag_it, self.k_pr), reverse = False)
+            self.save_list()
+        else:
+            return sorted(list_to_sort, key = itemgetter(self.k_dag_it, self.k_pr), reverse = False)
+
+    def get_training_data(self):
+        # shuffle data
+        shuffle(self.agg_list)
+        self.sort_agg_list()
+        if self.dag_it_num > 1:
+            train = self.agg_list[:self.len_train_set + self.len_agg_set * (self.dag_it_num - 1)]
+            num_images_found = len(train)
+        else:
+            train = self.agg_list[:self.len_train_set]
+            num_images_found = len(train)
+
+        for i in range(num_images_found,
+                       len(self.agg_list)):
+            if self.agg_list[i][self.k_quota_g] >= 0.07:
+                self.agg_list[i][self.k_dag_it] = self.dag_it_num
+                train.append(self.agg_list[i])
+                num_images_found += 1
+            if num_images_found >= self.num_imgs_to_train:
+                print('%d images for next DAgger Iteration found at index %d of total images %d'
+                      % (self.num_imgs_to_train, i, len(self.agg_list)))
+                break
+        self.images_evaluated = i
+        shuffle(train)
+        return train[:int(len(train) * self.train_perc)], train[int(len(train) * self.train_perc):]
+
+    def save_list(self, list_to_save = None, name = None):
+        if list_to_save is None:
+            with open(self.agg_list_name, "w") as f:
+                json.dump(self.agg_list, f)
+        else:
+            path = self.base_dir + self.dag_dir + '%02d/' % self.dag_it_num + name + '_%02d' % self.dag_it_num + '.json'
+            with open(path, "w") as f:
+                json.dump(list_to_save, f)
+        f.close()
+
+    def load_list(self):
+        with open(self.agg_list_name, "r") as f:
+            list_chunk = json.load(f)
+        f.close()
+        return list_chunk
